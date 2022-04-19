636839497d2b81c55a30325b03aa2a0ebaf785f2
Nicolas Reyland <nicolas@reyland.net>
2022-04-19 14:32:19 +0200

    workaround the url-shortener directory chown

--- discord-bot/bot-entrypoint.sh
+++ discord-bot/bot-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
-chown -R app:app /app /git-hooks /short-urls
+chown -R app:app /app /git-hooks
+if [ -d "/short-urls" ]; then
+    chown -R app:app /short-urls
+fi
 source /run/secrets/discord-bot-env
 source /py/bin/activate
 su app -c "DISCORD_BOT_TOKEN=\"$DISCORD_BOT_TOKEN\" DISCORD_CHANNEL_ID=\"$DISCORD_CHANNEL_ID\"  python3 /app/bot.py"
