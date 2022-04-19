37185a6e74e57db1bda9e4513c574179a801adc8
Nicolas Reyland <nicolas@reyland.net>
2022-04-19 12:35:01 +0200

    Added setup-secrets.sh

---
+++ setup-secrets.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# reading the information
+echo "[Discord Secrets]"
+echo -n "Enter your discord bot token : "
+read discord_bot_token
+echo -n "Enter the discord channel id for the bot messages : "
+read discord_channel_id
+echo "[Github Secret]"
+echo -n "Enter the github webhook secret : "
+read github_wh_secret
+# writing to the .env files
+printf "#!/bin/sh\nexport DISCORD_BOT_TOKEN=\"$discord_bot_token\"\nexport DISCORD_CHANNEL_ID=\"$discord_channel_id\"\n" > discord-bot/.env
+printf "#!/bin/sh\nexport GITHUB_WB_SECRET=\"$github_wh_secret\"\n" > ghwh-listener/.env
+
